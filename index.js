e893050b138c79c156b9c64c2664688a0aec1f9a
Xarch <rakhaarkhana98@gmail.com>
2025-05-26 20:14:13 +0700

    terintegrasi dengan front end

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cartRoutes = require("./routes/cartRoutes");
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Ganti dengan URL frontend kamu
+    origin: "https://tugas-uprak.vercel.app/", // Ganti dengan URL frontend kamu
   })
 ); // biar bisa diakses dari frontend
 
